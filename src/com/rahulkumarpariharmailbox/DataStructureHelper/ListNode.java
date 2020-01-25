87cd9375968f0854f128fac6047ecb43ec8ad144
Rahul Kumar Parihar <rahulkumarpariharmailbox@gmail.com>
2020-01-25 21:59:49 +0530

    Adding required method to ListNode class

--- src/com/rahulkumarpariharmailbox/DataStructureHelper/ListNode.java
+++ src/com/rahulkumarpariharmailbox/DataStructureHelper/ListNode.java
@@ -1,13 +1,53 @@
 package com.rahulkumarpariharmailbox.DataStructureHelper;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Definition for singly-linked list
  */
 public class ListNode {
-    int val;
-    ListNode next;
+    public int val;
+    public ListNode next;
 
-    ListNode(int x) {
+    public ListNode(int x) {
         val = x;
+    }
+
+    /**
+     * Create a new singly linked list
+     *
+     * @param input array of integers
+     * @return singly linked list
+     */
+    public static ListNode CreateLinkedListFromArray(int[] input){
+        ListNode listNode = null;
+        ListNode head = null;
+        for(int i: input){
+            if(listNode == null){
+                listNode = new ListNode(i);
+                head = listNode;
+            }else{
+                listNode.next = new ListNode(i);
+                listNode = listNode.next;
+            }
+        }
+
+        return head;
+    }
+
+    /**
+     * Converts singly linked list into List
+     *
+     * @param node Singly Linked List
+     * @return list containing singly linked list value
+     */
+    public static List<Integer> LinkedListToList(ListNode node){
+        List<Integer> list = new ArrayList<>();
+        while(node != null){
+            list.add(node.val);
+            node = node.next;
+        }
+        return list;
     }
 }
